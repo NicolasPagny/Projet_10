7d17ff29a3be7aacaf002af972314ab396e7d181
Nicolas Pagny <nicolas.pagny.pro@gmail.com>
2025-01-23 14:13:00 +0100

    1ère version recommander terminée

--- recommander.py
+++ recommander.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 from scipy.sparse import csr_matrix
 from scipy.sparse.linalg import svds
+from sklearn.preprocessing import MinMaxScaler
 
 ############################################################################################################
 # Fonctions internes (non utilisées avec azure fonction)
@@ -115,7 +116,8 @@ def _pred_svds(clicks: pd.DataFrame, n_factors:int = 5):
     predicted_rating = np.dot(np.dot(U, np.diag(sigma)), Vt)
     predicted_rating_norm = (predicted_rating - predicted_rating.min()) / (predicted_rating.max() - predicted_rating.min())
 
-    predicted_rating_norm_df = pd.DataFrame(predicted_rating_norm, columns=clicks_articles_matrix.columns, index=clicks_articles_matrix.index)
+    predicted_rating_norm_df = pd.DataFrame(predicted_rating_norm, columns=clicks_articles_matrix.columns)
+    predicted_rating_norm_df.index.name = "user_id"
 
     return predicted_rating_norm_df
 
@@ -123,7 +125,7 @@ def _pred_svds(clicks: pd.DataFrame, n_factors:int = 5):
 # Fonctions utilisées dans azure fonctions
 ############################################################################################################
 
-def most_popular_articles(clicks: pd.DataFrame, n: int):
+def most_popular_articles(clicks: pd.DataFrame, n: int = 5):
     """Obtenir les articles les plus populaires
     
     Parameters:
@@ -141,7 +143,7 @@ def most_popular_articles(clicks: pd.DataFrame, n: int):
     return most_popular_articles
 
 def cosine_similar_articles_per_user(user_id:int, clicks: pd.DataFrame, articles_embeddings: pd.DataFrame):
-    """Obtenir les articles les plus similaires pour chaque utilisateur
+    """Obtenir les articles les plus similaires pour un utilisateur donné
     
     Parameters:
     user_id: l'identifiants de l'utilisateur
@@ -149,7 +151,7 @@ def cosine_similar_articles_per_user(user_id:int, clicks: pd.DataFrame, articles
     articles_embeddings: les embeddings des articles
     """
     
-    # Obtenir les 5 articles les plus similaires pour l'utilisateur
+    # Obtenir les articles les plus similaires pour l'utilisateur
     article_id = _last_clicked_articles_user(user_id, clicks)["click_article_id"]
     similar_articles = _cosine_similar_articles(article_id, articles_embeddings)
     similar_articles_series = pd.Series(
@@ -169,7 +171,7 @@ def best_cosine_similar_articles_per_user(user_id: int, clicks: pd.DataFrame, ar
     """
 
     # Obtenir les n articles les plus similaires pour chaque utilisateur
-    article_id = _last_clicked_articles_user(id, clicks)["click_article_id"]
+    article_id = _last_clicked_articles_user(user_id, clicks)["click_article_id"]
     similar_articles = _best_cosine_similar_articles(article_id, articles_embeddings, n)
     best_similar_articles_series = pd.Series(
         {article_id: score for article_id, score in similar_articles}
@@ -189,9 +191,11 @@ def preds_svds_user(user_id: int, clicks: pd.DataFrame, n_factors:int = 5):
 
     # Obtenir les prédictions svds
     predicted_rating_norm = _pred_svds(clicks, n_factors)
-    return predicted_rating_norm.loc[user_id]
+    predicted_rating_norm_user = predicted_rating_norm.loc[user_id]
 
-def best_preds_svds_users(users_id: int, clicks: pd.DataFrame, min_clicks: int = 2, n_factors:int = 5, n: int = 5):
+    return predicted_rating_norm_user
+
+def best_preds_svds_user(user_id: int, clicks: pd.DataFrame, min_clicks: int = 2, n_factors:int = 5, n: int = 5):
     """Obtenir les meilleurs prédictions pour un utilisateur donné
     
     Parameters:
@@ -203,19 +207,10 @@ def best_preds_svds_users(users_id: int, clicks: pd.DataFrame, min_clicks: int =
     # Obtenir les prédictions svds
     predicted_rating_norm = _pred_svds(clicks, min_clicks, n_factors)
 
-    # Obtenir les meilleures prédictions pour les utilisateurs
-    best_preds_users = {}
-    for user_id in users_id:
-        if user_id in predicted_rating_norm.index:
-            if n is not None:   
-                user_preds = predicted_rating_norm.loc[user_id].nlargest(n)
-            else:
-                user_preds = predicted_rating_norm.loc[user_id]
-            best_preds_users[user_id] = user_preds
+    # Obtenir les meilleures prédictions pour l'utilisateur
+    return predicted_rating_norm.loc[user_id].nlargest(n)
 
-    return best_preds_users
-
-def hybrid_recommander(users_id: int, clicks: pd.DataFrame, articles_embeddings: pd.DataFrame, n_factors:int = 15):
+def hybrid_recommander(user_id: int, clicks: pd.DataFrame, articles_embeddings: pd.DataFrame, n_factors:int = 15, n: int = 5, user_min_clicks: int = 10, article_min_clicks: int = 270):
     """Recommander des articles à un utilisateur en utilisant une approche hybride
     
     Parameters:
@@ -224,16 +219,44 @@ def hybrid_recommander(users_id: int, clicks: pd.DataFrame, articles_embeddings:
     articles_embeddings: les embeddings des articles
     min_clicks: le nombre minimal de clics par utilisateur
     n: le nombre d'articles à recommander
+    user_min_clicks: le nombre minimal de clics par utilisateur
+    article_min_clicks: le nombre minimal de clics par article
     """
 
-    # Obtenir les n articles les plus similaires pour chaque utilisateur (Content Based)
-    content_based_articles = cosine_similar_articles_per_user(users_id, clicks, articles_embeddings)
+    user_clicks = clicks.loc[clicks["user_id"] == user_id].groupby("click_article_id").size().sum()
 
-    # Obtenir les n articles recommandés avec la factorisation de matrice SVD
-    collaborative_filtering_articles = preds_svds_users(users_id, clicks, n_factors)
-    index_to_keep = list(collaborative_filtering_articles.keys())
-    print(index_to_keep)
-    filtered_content_based_articles = {key: value for key, value in content_based_articles.items() if key in index_to_keep}
+    if user_clicks == 0:
+        return "most popular", most_popular_articles(clicks, n)
+    elif user_clicks < user_min_clicks:
+        return "best cosine similar", best_cosine_similar_articles_per_user(user_id, clicks, articles_embeddings, n)
+    else:
+        # Obtenir les n articles les plus similaires pour chaque utilisateur (Content Based)
+        content_based_articles = cosine_similar_articles_per_user(user_id, clicks, articles_embeddings)
 
+        # Obtenir les intereactions utilisateur-article à un format réduit
+        nb_clicks_articles = clicks.groupby("click_article_id").size().sort_values(ascending=False)
+        nb_clicks_users = clicks.groupby("user_id").size().sort_values(ascending=False)
 
-    return filtered_content_based_articles, collaborative_filtering_articles 
+        filtered_articles  = nb_clicks_articles[nb_clicks_articles >= user_min_clicks]
+        filtered_users = nb_clicks_users[nb_clicks_users >= article_min_clicks]
+        filtered_clicks = clicks[clicks["click_article_id"].isin(filtered_articles.index) & clicks["user_id"].isin(filtered_users.index)]
+
+        # Obtenir les n articles recommandés avec la factorisation de matrice SVD
+        collaborative_filtering_articles = preds_svds_user(user_id, filtered_clicks, n_factors)
+
+        # Garder les articles correspondant aux articles filtrés
+        index_to_keep = list(collaborative_filtering_articles.keys())
+        filtered_content_based_articles = pd.Series({key: value for key, value in content_based_articles.items() if key in index_to_keep})
+        filtered_content_based_articles.index.name = "click_article_id"
+
+        # Normaliser les deux recommandations afin de les combiner
+        scaler = MinMaxScaler()
+        cb_scaled = scaler.fit_transform(filtered_content_based_articles.values.reshape(-1, 1))
+        cf_scaled = scaler.fit_transform(collaborative_filtering_articles.values.reshape(-1, 1))
+
+        # Combiner les deux recommandations
+        hybrid_articles = cb_scaled * cf_scaled
+        hybrid_articles = pd.Series(hybrid_articles.ravel(), index=filtered_content_based_articles.index)
+        best_hybrid_articles = hybrid_articles.nlargest(n)
+
+        return "hybrid", best_hybrid_articles
